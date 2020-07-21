fa4df892cb475b3522ee00ae245063db8348f6dc
deepak.kumar <netmaildeepak@gmail.com>
2020-07-21 22:02:19 +0530

    commit till updates to the data csv. Notifier is left.

--- PullPrice.py
+++ PullPrice.py
@@ -9,19 +9,25 @@ from nsepy import get_history
 
 
 def get_investments():
+    # get all the portfolio files and store them in input directory.
+
     input_files = glob.glob('data/input/*.csv')
     li = []
 
+    # read and concat all input files
     for filename in input_files:
         concat_investments = pd.read_csv(filename, index_col=None, header=0)
         li.append(concat_investments)
 
+    # concat to a dataframe and clean up duplicates and extra columns
     investments = pd.concat(li, axis=0, ignore_index=True)
     investments.drop_duplicates(subset='Symbol', keep='first', inplace=True)
-    investments.set_index('Symbol', inplace = True)
-    investments.drop(investments.columns[2:], axis=1, inplace=True)
+    investments[['ticker','exchange']] = investments['Symbol'].str.split('.',n=1, expand=True)
+    investments.set_index('ticker', inplace=True)
+    investments.drop(investments.columns[:-1], axis=1, inplace=True)
     investments.to_csv('data/investments.csv')
     # investments will have the ticker and normalized peak / high
+
     return
 
 
@@ -33,7 +39,7 @@ def update_meta():
         if symbol in update_data.index:
             continue
         else:
-            update_data.loc[symbol] = [0,0,'']
+            update_data.loc[symbol] = [0,0,0,'']
 
     update_data.to_csv('data/data.csv')
 
@@ -45,21 +51,19 @@ def update_price():
     prev_day = curr_day - BDay(5)
     ticker_hist = pd.DataFrame()
 
-    symbol_list = pd.read_csv('data/data.csv', index_col='Symbol')
+    symbol_list = pd.read_csv('data/data.csv', index_col='symbol')
 
     for ticker in symbol_list.index:
         ticker_hist = nsepy.get_history(symbol=ticker, start=prev_day, end=curr_day)
         # check if the normalized high from ticker_hist is greater than the one recorded in investments
+        # update if the normalized close is higher than the recorded high
+        symbol_list.loc[ticker, 'close'] = ticker_hist['Close'].min()
+        symbol_list.loc[ticker, 'updated'] = datetime.date.today()
+        if ticker_hist['Close'].min() > symbol_list.loc[ticker,'high']:
+            symbol_list.loc[ticker, 'high'] = ticker_hist['Close'].min()
 
-        if ticker_hist['Close'].min() > symbol_list[ticker,'High']:
-            symbol_list[ticker, 'High'] = ticker_hist['Close'].min()
-        # the above code should update the high value from the min close. Check next time and remove this comment
-        #####$$$$$$%%%%%%&&&&&****
+    symbol_list.to_csv('data/data.csv')
 
-
-    # if yes, overwrite it in investments
-
-    #Check discrepancy
 
 def notify(investments):
     # Check diff between updated last closed and updated peak
